69a07cb70537f200489c6531f3890fb18e4db57c
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-10-13 19:30:16 -0500

    WIP: created all my Get query to DB

--- queries.sql
+++ queries.sql
@@ -1,13 +1,13 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
-
+SELECT * FROM Customers where postalcode = '1010';
 -- Find the phone number for the supplier with the id 11
-
+SELECT phone FROM Suppliers where supplierid = 11;
 -- List first 10 orders placed, sorted descending by the order date
-
+SELECT top 10 * FROM Orders order by orderdate desc;
 -- Find all customers that live in London, Madrid, or Brazil
-
+SELECT * FROM Customers where city in ('London', 'Madrid','Brazil') order by customerid asc;
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
