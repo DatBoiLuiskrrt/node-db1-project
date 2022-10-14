f35a0590f287f777382f81f99bf157c0a5a3ebbc
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-10-13 19:34:34 -0500

    Created insert statement

--- queries.sql
+++ queries.sql
@@ -9,7 +9,7 @@ SELECT top 10 * FROM Orders order by orderdate desc;
 -- Find all customers that live in London, Madrid, or Brazil
 SELECT * FROM Customers where city in ('London', 'Madrid','Brazil') order by customerid asc;
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
-
+insert into customers (customername, contactname, address, city, postalcode, country) values ("Alfredo Ramos Sex Shop", "Alfredo Ramos", "Avda. de la Constitucion 2426", "Mexico D.F.", "05024", "Mexico"); 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
