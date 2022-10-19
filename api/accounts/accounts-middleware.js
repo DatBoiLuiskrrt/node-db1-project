c0219fa27c7fdb97704101219913e75ee77c8e08
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-10-19 18:19:03 -0500

    WIP

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,13 +1,19 @@
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  // DO YOUR
+  console.log("checkAccountPayload");
+  next();
   // Note: you can either write "manual" validation logic
   // or use the Yup library (not currently installed)
 };
 
 exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+  // DO YOUR
+  console.log("checkAccountNameUnique");
+  next();
 };
 
 exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
+  // DO YOUR
+  console.log("checkAccountId");
+  next();
 };
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,42 +1,57 @@
 const router = require("express").Router();
+const md = require("./accounts-middleware");
+const Account = require("./accounts-model");
 
-router.get("/", (req, res, next) => {
+router.get("/", async (req, res, next) => {
   // DO YOUR MAGIC
   try {
-    res.json("Get accounts");
+    const accounts = await Account.getAll();
+    res.json(accounts);
   } catch (err) {
     next({ status: 422, message: "Asi nomas quedo ? que onda" });
   }
 });
 
-router.get("/:id", (req, res, next) => {
+router.get("/:id", md.checkAccountId, async (req, res, next) => {
   // DO YOUR MAGIC
   try {
-    res.json("Get accound by id");
+    const account = await Account.getById(req.params.id);
+    res.json(account);
   } catch (err) {
     next(err);
   }
 });
 
-router.post("/", (req, res, next) => {
-  // DO YOUR MAGIC
-  try {
-    res.json("Post account");
-  } catch (err) {
-    next(err);
+router.post(
+  "/",
+  md.checkAccountPayload,
+  md.checkAccountNameUnique,
+  (req, res, next) => {
+    // DO YOUR MAGIC
+    try {
+      res.json("Post account");
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
-router.put("/:id", (req, res, next) => {
-  // DO YOUR MAGIC
-  try {
-    res.json("Update account by ID");
-  } catch (err) {
-    next(err);
+router.put(
+  "/:id",
+  md.checkAccountId,
+  md.checkAccountPayload,
+  md.checkAccountNameUnique,
+  (req, res, next) => {
+    // DO YOUR MAGIC
+    try {
+      res.json("Update account by ID");
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", md.checkAccountId, (req, res, next) => {
   // DO YOUR MAGIC
   try {
     res.json("Delete accound by id");
